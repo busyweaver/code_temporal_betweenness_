0182991cfbfbe660fd98b7884d4ba666d3bc514f
mehdi naima <olmehdiol@gmail.com>
2023-04-20 15:01:57 +0200

    bench script

---
+++ src/bench.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+./btwBenchmark -f ../src/examples/primaryschool.csv -n 1 -b 1 -t 1
+./btwBenchmark -f ../src/examples/highschool_2012.csv -n 1 -b 1 -t 1
+./btwBenchmark -f ../src/examples/highschool_2011.csv -n 1 -b 1 -t 1
+./btwBenchmark -f ../src/examples/hospital_ward.csv -n 1 -b 1 -t 1
+./btwBenchmark -f ../src/examples/ht09.csv -n 1 -b 1 -t 1
+./btwBenchmark -f ../src/examples/workplace_2013.csv -n 1 -b 1 -t 1
