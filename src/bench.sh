1c8f725069f6a16b01641de93a9c9e04d6d6afae
mehdi naima <olmehdiol@gmail.com>
2023-07-16 13:32:31 +0200

    added time limit

--- src/bench.sh
+++ src/bench.sh
@@ -3,14 +3,14 @@
 #db = primaryschool.csv highschool_2012.csv highschool_2011.csv hospital_ward.csv ht09.csv workplace_2013.csv
 for args in $*
 do
-    ./btwBenchmark -f ${args} -n 1 -b 1 -t 1
+    timeout 7200s ./btwBenchmark -f ${args} -n 1 -b 1 -t 1
 done
 
 for args in $*
       do
          for j in 90 80 70 60 50 40 30 20 10
          do
-             ./btwBenchmark -f ${args} -n 1 -b 1 -p $j
+             timeout 7200s ./btwBenchmark -f ${args} -n 1 -b 1 -p $j
          done
       done
 
